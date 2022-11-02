0c6d85f8bdc8505dc9068b81b6a9b9fd1098aeaa
jcaraballo <jhompo@gmail.com>
2022-11-01 21:46:33 -0500

    Funciones Api Creadas y Database

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,58 +7,51 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+        return Book::all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
-        //
+        $request->validate(['title'=>'required']);
+
+        $book = new Book();
+        $book->title = $request->title;
+        //print_r($book);
+        $book->save();
+
+        return $book;
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Book $book)
     {
-        //
+        return $book;
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
-     */
+ 
+    
     public function update(Request $request, Book $book)
     {
-        //
+        $request->validate(['title'=>'required']);
+
+        $book->title = $request->title;
+        //print_r($book);
+        $book->save();
+
+        return $book;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
-     */
+ 
     public function destroy(Book $book)
     {
-        //
+        $book->delete();
+
+        return "REGISTRO [$book->title] ELIMINADO";
+        
+       // return response()->noContent();
     }
 }
